78d8a54bd582bbbf10673bbbb8c18a26f831f6f6
Muhammadzoir Mahmudov <kolen1470@gmail.com>
2024-03-16 23:12:43 +0500

    full version pokemon card

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-const api = "https:/pokeapi.co/api/v2/pokemon/";
-const btn = document.querySelector("#generate")
+/*
+const api = "https://pokeapi.co/api/v2/pokemon/";
+const generateBtn = document.querySelector("#generate")
 
-let randomNumber = Math.random(1000) + 1;
+let randomNumber = Math.trunc(Math.random() * 1000) + 1;
 
-function pudateUi(pokeInfo) {
+function upudateUi(pokeInfo) {
     console.log(pokeInfo);
     console.log("hp", pokeInfo.stats[0].base_stats);
     console.log("attack", pokeInfo.stats[1].base_stats);
@@ -15,6 +16,52 @@ function pudateUi(pokeInfo) {
 }
 
 async function getData(api) {
-    
+    const req = await fetch(api);
+    const data = req.json();
+
+    return data;
 }
+
+getData(api + randomNumber).then((data) => upudateUi(data)).catch((error) => console.log(error));
+
+
+
+generateBtn.addEventListener("click" , () => {
+    randomNumber = Math.trunc(Math.random() * 1000) + 1;
+    const api = "https://pokeapi.co/api/v2/pokemon/" + randomNumber;
+    //getData(api)
+    getData(api).then((data) => upudateUi(data)).catch((error) => console.log(error));
+
+})
+*/
+const api = "https://pokeapi.co/api/v2/pokemon/";
+const generateBtn = document.querySelector("#generate");
+let randomNumber = Math.trunc(Math.random() * 1000) + 1;
+
+function updateUi(pokeInfo) {
+    document.getElementById("pokemon-name").textContent = pokeInfo.name;
+    document.getElementById("pokemon-hp").textContent = pokeInfo.stats[0].base_stat;
+    document.getElementById("pokemon-attack").textContent = pokeInfo.stats[1].base_stat;
+    document.getElementById("pokemon-defense").textContent = pokeInfo.stats[2].base_stat;
+    document.getElementById("pokemon-speed").textContent = pokeInfo.stats[5].base_stat;
+    document.getElementById("pokemon-type").textContent = pokeInfo.types[0].type.name;
+    document.getElementById("pokemon-image").src = pokeInfo.sprites.other.dream_world.front_default ?? pokeInfo.sprites.front_default;
+}
+
+
+function getData(api) {
+    return fetch(api).then(response => response.json());
+}
+
+getData(api + randomNumber)
+    .then(data => updateUi(data))
+    .catch(error => console.log(error));
+
+generateBtn.addEventListener("click", () => {
+    randomNumber = Math.trunc(Math.random() * 1000) + 1;
+    const apiUrl = api + randomNumber;
+    getData(apiUrl)
+        .then(data => updateUi(data))
+        .catch(error => console.log(error));
+});
 
